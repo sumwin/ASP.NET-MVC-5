8fb7bb6e880e93533da42260f273efd3cfac505d
liew sum win <sumwin@outlook.com>
2018-10-05 23:13:42 +0800

    Initial DbContext and DB migration

---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Superman"},
+                new Movie {Id = 2, Name = "Batman"}
+            };
+        }
+
+        //GET: Movies /Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Superman" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
