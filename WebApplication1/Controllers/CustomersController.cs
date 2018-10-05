15e5000aa39827b4ea8d9bbd97163f2144bb7e3e
liew sum win <sumwin@outlook.com>
2018-10-05 18:45:51 +0800

    Working Customer link

---
+++ WebApplication1/Controllers/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id = 1, Name = "Sum Win"},
+                new Customer {Id = 2, Name = "Wai Yee"}
+            };
+        }
+    }
+}
